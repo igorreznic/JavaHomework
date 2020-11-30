867f2b5700670fe011fcae1faa2df123e5ec5fbb
irezn <i.reznic@gmail.com>
2020-11-30 20:13:35 +0200

    added MovieQuote

--- src/com/tekwill/learning/basics/MovieQuote.java
+++ src/com/tekwill/learning/basics/MovieQuote.java
@@ -2,6 +2,6 @@ package com.tekwill.learning.basics;
 
 public class MovieQuote {
     public static void main(String[] args) {
-        System.out.println("Movie Quote");
+        System.out.println("Just a Movie Quote");
     }
 }
