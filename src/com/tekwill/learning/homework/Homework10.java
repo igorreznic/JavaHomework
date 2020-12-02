394e61082fd4eb5deeaa3fa60780f932ea935508
irezn <i.reznic@gmail.com>
2020-12-02 18:05:25 +0200

    added Homework10

---
+++ src/com/tekwill/learning/homework/Homework10.java
@@ -0,0 +1,7 @@
+package com.tekwill.learning.homework;
+
+public class Homework10 {
+    public static void main(String[] args) {
+        System.out.println("A quoted String is \n'much' better if you learn \nthe rules of \"escape sequenses.\"\n \nAlso, \"\" represents an empty String. \n Don't forget: use \\\" instead of \" ! \n '' is not the same as \"");
+    }
+}
