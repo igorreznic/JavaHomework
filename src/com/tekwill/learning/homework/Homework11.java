78fcb4af619e422485c37cdf67521b445ab1431b
irezn <i.reznic@gmail.com>
2020-12-02 18:22:06 +0200

    added Homework11

---
+++ src/com/tekwill/learning/homework/Homework11.java
@@ -0,0 +1,7 @@
+package com.tekwill.learning.homework;
+
+public class Homework11 {
+    public static void main(String[] args) {
+        System.out.println("  \\/ \n \\\\// \n\\\\\\/// \n///\\\\\\ \n //\\\\ \n  /\\");
+    }
+}
