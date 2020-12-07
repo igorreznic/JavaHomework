2c05dfdb53c1c360e4aad76d3a73705f5b9546ec
irezn <i.reznic@gmail.com>
2020-12-07 18:18:43 +0200

    added FightSong Class

---
+++ src/com/tekwill/learning/basics/FightSong.java
@@ -0,0 +1,10 @@
+package com.tekwill.learning.basics;
+
+public class FightSong {
+    public static void main(String[] args) {
+        System.out.println("Go, team, go! \nYou can do it. \n \n" +
+                "Go, team, go! \nYou can do it. \nYou\'re the best, \nIn the West. \nGo, team, go! \nYou can do it.\n\n" +
+                "Go, team, go! \nYou can do it. \nYou\'re the best, \nIn the West. \nGo, team, go! \nYou can do it.\n\n" +
+                "Go, team, go! \nYou can do it.");
+    }
+}
