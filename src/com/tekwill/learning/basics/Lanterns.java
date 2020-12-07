53b54b7dbc93b1ecc0fcdcde76f5a41397925afa
irezn <i.reznic@gmail.com>
2020-12-07 18:31:05 +0200

    added Lanterns Class

---
+++ src/com/tekwill/learning/basics/Lanterns.java
@@ -0,0 +1,12 @@
+package com.tekwill.learning.basics;
+
+public class Lanterns {
+    public static void main(String[] args) {
+        System.out.println("    *****\n  ********* \n*************\n\n" +
+                "    *****\n  ********* \n*************\n" +
+                "* | | | | | * \n************* \n" +
+                "    *****\n  ********* \n*************\n\n" +
+                "    *****\n  ********* \n*************\n" +
+                "    ***** \n* | | | | | * \n* | | | | | * \n    ***** \n    *****");
+    }
+}
