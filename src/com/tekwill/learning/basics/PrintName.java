f0c6f3d1df90ed83c5a9c97dcee677d03861a049
irezn <i.reznic@gmail.com>
2020-12-07 18:05:30 +0200

    added PrintName Class

---
+++ src/com/tekwill/learning/basics/PrintName.java
@@ -0,0 +1,11 @@
+package com.tekwill.learning.basics;
+
+public class PrintName {
+    public static void main(String[] args) {
+        System.out.println("IIIII    GGGGG      OOOOO      RRRRR \n" +
+                "  I     G     G    O     O    R     R \n  I     G          O     O    R     R \n" +
+                "  I     G          O     O    RRRRRR \n  I     G    GG    O     O    R R \n" +
+                "  I     G     G    O     O    R   R \nIIIII    GGGGG      OOOOO     R     R");
+    }
+
+}
