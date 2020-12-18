6cc0d1e9e6dd7bf691c5e91be325be9a9cdcf2bc
irezn <i.reznic@gmail.com>
2020-12-18 14:52:07 +0200

    modified BodyMassIndex Class

--- src/com/tekwill/learning/basics/io/reading/BodyMassIndex.java
+++ src/com/tekwill/learning/basics/io/reading/BodyMassIndex.java
@@ -3,10 +3,11 @@ package com.tekwill.learning.basics.io.reading;
 import java.util.Scanner;
 
 public class BodyMassIndex {
-    public static void main (String[] args){
-        Scanner scanner =new Scanner(System.in);
-        final float INCHES_TO_METERS_CONVERSION_RATIO = 0.0254F;
-        final float KG_TO_POUNDS_CONVERSION_RATIO = 2.205F;
+    public static final float INCHES_TO_METERS_CONVERSION_RATIO = 0.0254F;
+    public static final float KG_TO_POUNDS_CONVERSION_RATIO = 2.205F;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Please enter Your height in feet and inches:\n" +
                 "Firstly, enter feet:");
