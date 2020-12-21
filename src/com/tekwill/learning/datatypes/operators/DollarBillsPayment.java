d62e75ff572cb99afbbc3a865f97ba5acf309457
irezn <i.reznic@gmail.com>
2020-12-22 00:55:31 +0200

    made some changes

--- src/com/tekwill/learning/datatypes/operators/DollarBillsPayment.java
+++ src/com/tekwill/learning/datatypes/operators/DollarBillsPayment.java
@@ -3,30 +3,14 @@ package com.tekwill.learning.datatypes.operators;
 import java.util.Scanner;
 
 public class DollarBillsPayment {
-    public static int Count$20Bills(int amount) {
-        return amount / 20;
-    }
-
-    public static int CountRemaining$10Bills(int amount) {
-        return (amount % 20) / 10;
-    }
-
-    public static int CountRemaining$5Bills(int amount) {
-        return ((amount % 20) % 10) / 5;
-    }
-
-    public static int CountRemaining$1Bills(int amount) {
-        return ((amount % 20) % 10) % 5;
-    }
 
     public static void main(String... args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter Dollar amount:");
         int amount = scanner.nextInt();
-        System.out.println("$20 Bills: " + Count$20Bills(amount) +
-                "\n$10 Bills: " + CountRemaining$10Bills(amount) +
-                "\n $5 Bills: " + CountRemaining$5Bills(amount) +
-                "\n $1 Bills: " + CountRemaining$1Bills(amount));
-
+        System.out.println("$20 Bills: " + amount / 20 +
+                "\n$10 Bills: " + (amount % 20) / 10 +
+                "\n $5 Bills: " + ((amount % 20) % 10) / 5 +
+                "\n $1 Bills: " + ((amount % 20) % 10) % 5);
     }
 }
