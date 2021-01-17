0bc923ae7bf870e0390475e66919672eab38fa29
irezn <i.reznic@gmail.com>
2021-01-17 18:19:03 +0200

    added RockPaperScissorsGame Class

---
+++ src/com/tekwill/learning/homework8/RockPaperScissorsGame.java
@@ -0,0 +1,66 @@
+package com.tekwill.learning.homework8;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class RockPaperScissorsGame {
+
+    public static int getUserAnswer() {
+        System.out.println("Enter Your choice: press       [1] for Rock,        [2] for Paper         or [3] for Scissors");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+
+    public static int getComputerAnswer() {
+        Random random = new Random();
+        return random.nextInt(3) + 1;
+    }
+
+    public static void playAgain() {
+        System.out.println("\nPress [Y] if you want to play again, or any key to EXIT the game.");
+        Scanner scanner = new Scanner(System.in);
+        String response = scanner.next().toUpperCase();
+        if (response.equalsIgnoreCase("Y")) {
+            play();
+        } else {
+            System.out.println("Game Over");
+        }
+    }
+
+    public static void play() {
+        int computerAnswer = getComputerAnswer();
+        int userAnswer = getUserAnswer();
+
+        checkAnswers(computerAnswer, userAnswer);
+    }
+
+    public static void checkAnswers(int computerChoice, int userChoice) {
+        if (computerChoice == userChoice) {
+            System.out.println("Tie!!! You both chose the same element\n[" + computerChoice + "] : [" + userChoice + "]");
+            playAgain();
+        } else if (computerChoice == 1 && userChoice == 3) {
+            System.out.println("You Lost!!!     You [Scissors]   :   [Rock]  Computer");
+            playAgain();
+        } else if (computerChoice == 2 && userChoice == 1) {
+            System.out.println("You Lost!!!     You [Rock]   :   [Paper]  Computer ");
+            playAgain();
+        } else if (computerChoice == 3 && userChoice == 2) {
+            System.out.println("You Lost!!!     You  [Paper]   :   [Scissors]  Computer");
+            playAgain();
+        } else if (computerChoice == 1 && userChoice == 2) {
+            System.out.println("You Won!!!     You [Paper]   :  [Rock]  Computer");
+            playAgain();
+        } else if (computerChoice == 2 && userChoice == 3) {
+            System.out.println("You Won!!!     You [Scissors]   :  [Paper]  Computer");
+            playAgain();
+        } else if (computerChoice == 3 && userChoice == 1) {
+            System.out.println("You Won!!!     You  [Rock]   :  [Scissors]  Computer");
+            playAgain();
+        } else {
+            System.out.println("Wrong input");
+            playAgain();
+        }
+    }
+
+
+}
---
+++ src/com/tekwill/learning/homework8/RockPaperScissorsGameDemo.java
@@ -0,0 +1,10 @@
+package com.tekwill.learning.homework8;
+
+public class RockPaperScissorsGameDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Let's PLAY the Rock, Paper, Scissors game:\n");
+
+        RockPaperScissorsGame.play();
+    }
+}
