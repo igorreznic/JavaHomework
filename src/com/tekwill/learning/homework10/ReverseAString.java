97012c4e55196210d957ff52aaabcf92bdad8a70
irezn <i.reznic@gmail.com>
2021-01-21 17:44:10 +0200

    added ReverseAString Class

---
+++ src/com/tekwill/learning/homework10/ReverseAString.java
@@ -0,0 +1,14 @@
+package com.tekwill.learning.homework10;
+
+import java.util.Scanner;
+
+public class ReverseAString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String input = scanner.nextLine();
+        for (int i = input.length() - 1; i >= 0; i--) {
+            System.out.print(input.charAt(i));
+        }
+    }
+}
